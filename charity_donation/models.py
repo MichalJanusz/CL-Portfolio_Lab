0c944d4ad1656d96bb7a832aa52350dc0e57c725
Aknifee <janusz.j.michal@gmail.com>
2020-02-12 17:59:05 +0100

    Created models Institution and Category

--- charity_donation/models.py
+++ charity_donation/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+
+TYPE_CHOICES = (
+    (1, 'fundacja'),
+    (2, 'organizacja pozarządowa'),
+    (3, 'zbiórka lokalna'),
+)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    type = models.IntegerField(choices=TYPE_CHOICES, default=1)
+    categories = models.ManyToManyField('Category')
