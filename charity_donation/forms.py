5d07c61db6965ad53a010531c869a8c3741ba142
Aknifee <janusz.j.michal@gmail.com>
2020-02-22 11:15:00 +0100

    admin panel link added, used get_user_model in registration form

--- charity_donation/forms.py
+++ charity_donation/forms.py
@@ -1,12 +1,13 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm
 
-from .models import User, Donation, Category, Institution
+from .models import Donation, Category, Institution
 
 
 class RegisterForm(forms.ModelForm):
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ['first_name', 'last_name', 'email', 'password']
         widgets = {
             'last_name': forms.TextInput(attrs={'placeholder': 'Nazwisko'}),
