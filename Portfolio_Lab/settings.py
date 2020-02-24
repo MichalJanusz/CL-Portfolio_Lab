843106ab5504ccc979284c50dcd81a980f6de4af
Aknifee <janusz.j.michal@gmail.com>
2020-02-24 18:11:58 +0100

    added django extensions and changed admin url

--- Portfolio_Lab/settings.py
+++ Portfolio_Lab/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'charity_donation'
+    'charity_donation',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
